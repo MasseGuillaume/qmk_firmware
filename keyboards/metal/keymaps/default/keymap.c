a870feb9bb27616ac123123fb95c0c1cbfee2f35
Guillaume Masse <bob@guillaumes-air.home>
2024-08-21 19:09:34 -0400

    metal

---
+++ keyboards/metal/keymaps/default/keymap.c
@@ -0,0 +1,15 @@
+#include QMK_KEYBOARD_H
+
+enum layers
+{
+    _ALPHA = 0
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_ALPHA] = LAYOUT(
+        KC_1,         KC_2,    KC_ESC,    KC_P,    KC_Y,              KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
+        KC_A,         KC_O,    KC_E,      KC_U,    KC_I,              KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
+        KC_TAB,       KC_Q,    KC_J,      KC_K,    KC_X,              KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,
+                               KC_DELETE, KC_BSPC, KC_4,              KC_ENTER, KC_SPACE, KC_5
+    ),
+};
